8a2ac15e7874563235947ef5a04a9bd28c86628e
pawelpaszkowski <p4szk0wski@gmail.com>
2025-09-28 01:27:32 +0200

    init commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, output, Output} from '@angular/core';
+import { User } from './user.model';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+}) 
+export class UserComponent {
+
+  @Input({required: true})
+  user!: User;
+  @Input({required: true})
+  selected!: boolean;
+
+  @Output()
+  select = new EventEmitter<string>();
+
+  get imagePath() {
+    return `assets/users/${this.user.avatar}`;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
